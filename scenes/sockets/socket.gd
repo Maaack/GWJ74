dd7d140bee294a942d2064e637a0acdb9a792d6f
Marek Belski <marek.belski@gmail.com>
2024-10-19 09:33:45 -0700

    Adds particles to sockets.

--- scenes/sockets/socket.gd
+++ scenes/sockets/socket.gd
@@ -13,11 +13,24 @@ signal gate_removed
 			if inserted_gate is LogicGate:
 				inserted_gate.global_position = global_position
 				inserted_gate.connected_socket = self
-				if $GateInsertedStreamPlayer2D.is_inside_tree():
-					$GateInsertedStreamPlayer2D.play()
+				_animate_gate_insert()
 				gate_inserted.emit(inserted_gate)
 			else:
 				gate_removed.emit()
+
+@onready var particle_2ds : Array[GPUParticles2D] = [
+	$GPUParticles2D,
+	$GPUParticles2D2,
+	$GPUParticles2D3,
+	$GPUParticles2D4
+]
+
+func _animate_gate_insert():
+	if $GateInsertedStreamPlayer2D.is_inside_tree():
+		$GateInsertedStreamPlayer2D.play()
+	for particle_2d in particle_2ds:
+		if particle_2d.is_inside_tree():
+			particle_2d.emitting = true
 
 func insert(gate : LogicGate):
 	inserted_gate = gate
